7aa440f6f51b9482b21b4c262a210f3aad56f7c7
Yashank Bhola <64089101+yashankxy@users.noreply.github.com>
2022-06-19 16:58:56 -0400

    Adding Testcases

--- src/test/java/ca/utoronto/utm/mcs/AppTest.java
+++ src/test/java/ca/utoronto/utm/mcs/AppTest.java
@@ -1,17 +1,73 @@
 package ca.utoronto.utm.mcs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 // TODO Please Write Your Tests For CI/CD In This Class. You will see
     // these tests pass/fail on github under github actions.    
 public class AppTest {
 
+    @BeforeAll
+    private static void setServer(){
+        // TODO
+    }
+
+    final static String API_URL = "http://localhost:8080";
+    final static String Path = "/api/v1";
+    private static HttpResponse<String> sendRequest(String endpoint, String method, String reqBody) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL + Path + "/"+ endpoint))
+//                .version(HttpClient.Version.HTTP_1_1)
+                .method(method, HttpRequest.BodyPublishers.ofString(reqBody))
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
     //Add
     //--------------------------------
     @Test
-    public void addActorPass() { //200
-        assertTrue(true);
+    @Order(1)
+    public void addActorPass() throws JSONException, IOException, InterruptedException { //200
+//        URL url = new URL(API_URL+"/api/v1/addActor");
+        JSONObject rq = new JSONObject();
+        rq.put("name", "Jack");
+        rq.put("actorId", "20");
+        HttpResponse<String> confirmRes = sendRequest("addActor", "PUT", rq.toString());
+        assertEquals(HttpURLConnection.HTTP_OK, confirmRes.statusCode());
+//        assertEquals("2\n", confirmRes.body());
+//        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//        httpURLConnection.setDoOutput(true);
+//        httpURLConnection.setRequestMethod("PUT");
+//        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+//        OutputStream os = httpURLConnection.getOutputStream();
+//        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+//        outputStreamWriter.write(rq.toString());
+//        outputStreamWriter.close();
+//        os.close();
+//        OutputStreamWriter out = new OutputStreamWriter(
+//                httpURLConnection.getOutputStream());
+//        out.write("Resource content");
+//        out.close();
+//        httpURLConnection.getInputStream();
+//        httpURLConnection.connect();
+//        assertEquals(200, httpURLConnection.getResponseCode());
     }
     @Test
     public void addActorFail() { //400
